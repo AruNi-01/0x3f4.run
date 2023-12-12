951c42fb7752fea3818e8b161639b9275830b497
AarynLu <hello@0x3f4.run>
2023-12-13 02:16:42 +0800

    ✨ feat: add contact-page text and contact ways

--- components/contact-page/ContactWay.tsx
+++ components/contact-page/ContactWay.tsx
@@ -1,34 +1,112 @@
-import React from "react";
-import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
 import { contactWays } from "@/config/contact-ways";
-import { EmailIcon } from "../icons";
+import {
+  Popover,
+  PopoverContent,
+  PopoverHandler,
+} from "@material-tailwind/react";
+import { Button } from "@nextui-org/button";
+import { Link } from "@nextui-org/link";
+import { Image, Snippet } from "@nextui-org/react";
+import React, { MouseEvent } from "react";
 
 export default function ContactWay() {
+  // use boolean[] to control contact is popover?
+  const [popoverState, setPopoverState] = React.useState<boolean[]>(
+    contactWays.map(() => false)
+  );
+
+  const onMouseEnterHandle = (id: string, hasPopover: boolean) => {
+    if (!hasPopover) return;
+    return (event: MouseEvent<HTMLDivElement>) => {
+      return setPopoverState((prevStates) => {
+        return prevStates.map((state, index) =>
+          contactWays[index].id === id ? true : state
+        );
+      });
+    };
+  };
+
+  const onMouseLeaveHandle = (id: string) => {
+    return (event: MouseEvent<HTMLDivElement>) => {
+      return setPopoverState((prevStates) => {
+        return prevStates.map((state, index) =>
+          contactWays[index].id === id ? false : state
+        );
+      });
+    };
+  };
+
   return (
-    <div className="flex justify-start w-full flex-col">
-      <Tabs
-        aria-label="Dynamic tabs"
-        color="primary"
-        variant="solid"
-        items={contactWays}
-      >
-        {(item) => (
-          <Tab
-            key={item.name}
-            title={
-              <div className="flex items-center space-x-2">
-                <img src={item.icon} alt={item.name} className="w-5 h-5" />
-                {/* <EmailIcon /> */}
-                <span>{item.name}</span>
-              </div>
-            }
+    <div className="flex items-start justify-center gap-3 mt-2">
+      {contactWays.map((contact) => (
+        <Popover
+          key={contact.name}
+          open={popoverState.find(
+            (_, index) => contactWays[index].id === contact.id
+          )}
+          handler={(isOpen) => {
+            if (!contact.hasPopover) return;
+            setPopoverState((prevStates) =>
+              prevStates.map((state, index) =>
+                contactWays[index].id === contact.id ? isOpen : state
+              )
+            );
+          }}
+          placement="bottom"
+          animate={{
+            mount: { scale: 1, y: 0, transition: { duration: 0.5 } },
+            unmount: {
+              scale: 0,
+              y: -25,
+              transition: { delay: 0.5, duration: 0.8 },
+            },
+          }}
+        >
+          <PopoverHandler
+            onMouseEnter={onMouseEnterHandle(contact.id, contact.hasPopover)}
+            onMouseLeave={onMouseLeaveHandle(contact.id)}
           >
-            <Card>
-              <CardBody>{item.link}</CardBody>
-            </Card>
-          </Tab>
-        )}
-      </Tabs>
+            <Button
+              as={Link}
+              isExternal={contact.link ? true : false}
+              href={contact.link ? contact.link : ""}
+              color={contact.link ? "primary" : "default"}
+              variant="faded"
+              className="text-neutral-800 dark:text-neutral-300"
+            >
+              <img src={contact.icon} alt={contact.name} />
+              {contact.name}
+            </Button>
+          </PopoverHandler>
+          <PopoverContent
+            onMouseEnter={onMouseEnterHandle(contact.id, contact.hasPopover)}
+            onMouseLeave={onMouseLeaveHandle(contact.id)}
+            className="border-none bg-transparent shadow-none w-48"
+          >
+            <div className="flex-col items-center gap-4 -mt-1 w-48">
+              {contact.searchText ? (
+                <Snippet
+                  tooltipProps={{
+                    color: "foreground",
+                    content: "Copy to add",
+                    disableAnimation: true,
+                    placement: "right",
+                    closeDelay: 0,
+                  }}
+                  symbol="ID: "
+                  radius="none"
+                  className="w-full rounded-tl-xl rounded-tr-xl"
+                >
+                  {contact.searchText}
+                </Snippet>
+              ) : (
+                ""
+              )}
+              {contact.qrCode ? <Image src={contact.qrCode} radius="none" className="rounded-bl-xl rounded-br-xl" /> : ""}
+            </div>
+          </PopoverContent>
+        </Popover>
+      ))}
     </div>
   );
 }
--- config/contact-ways.ts
+++ config/contact-ways.ts
@@ -1,17 +1,25 @@
 export const contactWays = [
   {
+    id: "1",
     name: "Wechat",
     icon: "/contact/wechat.svg",
-    link: "",
+    hasPopover: true,
+    qrCode: "/contact/wechat-qrcode.jpg",
+    searchText: "Lrr_0x3F4",
   },
   {
+    id: "2",
     name: "QQ",
     icon: "/contact/qq.svg",
-    link: "",
+    hasPopover: true,
+    qrCode: "/contact/qq-qrcode.jpg",
+    searchText: "1298911600",
   },
   {
+    id: "3",
     name: "Twitter",
     icon: "/contact/twitter.svg",
     link: "https://twitter.com/AruNi_Lu",
-  }
+    hasPopover: false,
+  },
 ];
