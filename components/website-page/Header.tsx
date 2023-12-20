8cde20f7ef7e442446af52c4e745b0b827e7f387
AarynLu <hello@0x3f4.run>
2023-12-20 21:01:31 +0800

    feat: ✨ website page done

--- components/website-page/Header.tsx
+++ components/website-page/Header.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/link";
+import { Navbar } from "@nextui-org/navbar";
 import { ThemeSwitch } from "../home-page/theme-switch";
 import { BackIcon } from "../icons";
-import { Navbar } from "@nextui-org/navbar";
-import Lottie from "lottie-react";
-import ghost from "@/public/lottie/ghost.json";
 
 export default function Header() {
   return (
